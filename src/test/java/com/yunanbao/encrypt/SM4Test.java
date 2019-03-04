89a3479e8b11783815fde0fbc0bd77088dc08cee
zhaoming0018 <1786796646@qq.com>
2019-03-04 12:32:49 +0800

    delete all Chinese content

--- src/test/java/com/yunanbao/encrypt/SM4Test.java
+++ src/test/java/com/yunanbao/encrypt/SM4Test.java
@@ -29,18 +29,18 @@ public class SM4Test {
         byte[] plainText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".getBytes();
         SM4Utils sm4 = new SM4Utils();
 
-        System.out.println("CBC模式");
-        System.out.println("明文: ");
+        System.out.println("CBC mode");
+        System.out.println("plain text:");
         System.out.println(Util.printHexFromByte(plainText));
         sm4.setSecretKey(key);
         sm4.setIv(key);
         byte[] cipherText = sm4.encryptDataCBC(plainText);
-        System.out.println("密文是：");
+        System.out.println("cipher text:");
         System.out.println(Util.printHexFromByte(cipherText));
         sm4.setSecretKey(key);
         sm4.setIv(key);
         byte[] decrypt = sm4.decryptDataCBC(cipherText);
-        System.out.println("解密: ");
+        System.out.println("decode :");
         System.out.println(Util.printHexFromByte(decrypt));
         assert Arrays.equals(plainText, decrypt);
     }
@@ -53,18 +53,18 @@ public class SM4Test {
         byte[] plainText = "1234567890!@#$%^&*()_+-={}[]<>《》".getBytes();
         SM4Utils sm4 = new SM4Utils();
 
-        System.out.println("CBC模式");
-        System.out.println("明文: ");
+        System.out.println("CBC mode");
+        System.out.println("plain text: ");
         System.out.println(Util.printHexFromByte(plainText));
         sm4.setSecretKey(key);
         sm4.setIv(key);
         byte[] cipherText = sm4.encryptDataCBC(plainText);
-        System.out.println("密文是：");
+        System.out.println("cipher text:");
         System.out.println(Util.printHexFromByte(cipherText));
         sm4.setSecretKey(key);
         sm4.setIv(key);
         byte[] decrypt = sm4.decryptDataCBC(cipherText);
-        System.out.println("解密: ");
+        System.out.println("decode : ");
         System.out.println(Util.printHexFromByte(decrypt));
         assert Arrays.equals(plainText, decrypt);
     }
@@ -77,18 +77,18 @@ public class SM4Test {
         byte[] plainText = "第一章总纲第一条中华人民共和国是工人阶级领导的、以工农联盟为基础的人民民主专政的社会主义国家。社会主义制度是中华人民共和国的根本制度。中国共产党领导是中国特色社会主义最本质的特征。禁止任何组织或者个人破坏社会主义制度。第二条中华人民共和国的一切权力属于人民。人民行使国家权力的机关是全国人民代表大会和地方各级人民代表大会。人民依照法律规定，通过各种途径和形式，管理国家事务，管理经济和文化事业，管理社会事务".getBytes();
         SM4Utils sm4 = new SM4Utils();
 
-        System.out.println("CBC模式");
-        System.out.println("明文: ");
+        System.out.println("CBC mode");
+        System.out.println("plain text: ");
         System.out.println(Util.printHexFromByte(plainText));
         sm4.setSecretKey(key);
         sm4.setIv(key);
         byte[] cipherText = sm4.encryptDataCBC(plainText);
-        System.out.println("密文是：");
+        System.out.println("cipher text:");
         System.out.println(Util.printHexFromByte(cipherText));
         sm4.setSecretKey(key);
         sm4.setIv(key);
         byte[] decrypt = sm4.decryptDataCBC(cipherText);
-        System.out.println("解密: ");
+        System.out.println("decode :");
         System.out.println(Util.printHexFromByte(decrypt));
         assert Arrays.equals(plainText, decrypt);
     }
